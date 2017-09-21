0906c6ac639436282616f1c94348ca92c9af452d
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-09-20 15:45:24 -1000

    finished spendMana

--- constructors.js
+++ constructors.js
@@ -114,6 +114,14 @@ Spellcaster.prototype.inflictDamage = function(damage){
    * @param  {number} cost      The amount of mana to spend.
    * @return {boolean} success  Whether mana was successfully spent.
    */
+Spellcaster.prototype.spendMana = function(cost){
+  if(this.mana >= cost){
+    this.mana -= cost;
+    return true;
+  }else{
+    return false;
+  }
+}
 
   /**
    * @method invoke
